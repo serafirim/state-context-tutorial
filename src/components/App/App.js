f9be9eca8b0a6c6f5bc8ef13fd8cebdaaa3f9fc2
Serafirim <serafirim@gmail.com>
2023-02-01 23:45:51 -0600

    Push 1.0.1

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,32 @@
+/*
+::::::::::::::::::[ NOTES ]:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+* Context is an interface for sharing information. Many applications have some universal information they 
+  need to share across the application, such as user preferences, theming information, and site-wide app
+  changes. 
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+*/
+
 import './App.css'
+import Navigation from '../Navigation/Navigation'
+import SaladMaker from '../SaladMaker/SaladMaker'
+import UserContext from '../User/User'
+
+const user = {
+  name: 'Seri',
+  favorites: [
+    'apple',
+    'avocado',
+    'carrot',
+  ]
+}
 
 function App() {
   return (
     <>
-
+      <UserContext.Provider value={user}>
+          <Navigation />
+          <SaladMaker />
+      </UserContext.Provider>
     </>
   )
 }
